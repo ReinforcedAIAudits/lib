19be5b77ee1be59849ff62f8be923964463e74dc
Andy Smith <andy@unique.network>
2025-03-26 07:41:10 +0100

    Simple relayer client

---
+++ solidity_audit_lib/relayer_client/client.py
@@ -0,0 +1,57 @@
+import requests
+
+from solidity_audit_lib.messaging import KeypairType
+from solidity_audit_lib.relayer_client.relayer_types import (
+    RelayerMessage, RegisterParams, RegisterMessage, MinerStorage, ValidatorStorage, StorageMessage, TaskModel,
+    PerformAuditMessage
+)
+
+
+__all__ = ['RelayerClient']
+
+
+class RelayerClient(object):
+    def __init__(self, relayer_url: str, network_id: int, subnet_uid: int):
+        self.relayer_url = relayer_url
+        self.network_id = network_id
+        self.subnet_uid = subnet_uid
+        self._id = 0
+
+    def _call_rpc(self, method: str, params: dict):
+        self._id += 1
+        return requests.post(f'{self.relayer_url}/api/jsonrpc', json={
+            "jsonrpc": "2.0", "id": self._id, "method": method, "params": params
+        }).json()
+
+    def get_miners(self, signer: KeypairType):
+        msg = RelayerMessage(network_id=self.network_id, subnet_uid=self.subnet_uid)
+        msg.sign(signer)
+        return self._call_rpc('metagraph.get_miners', msg.model_dump())
+
+    def get_validators(self, signer: KeypairType):
+        msg = RelayerMessage(network_id=self.network_id, subnet_uid=self.subnet_uid)
+        msg.sign(signer)
+        return self._call_rpc('metagraph.get_validators', msg.model_dump())
+
+    def register_axon(self, signer: KeypairType, params: RegisterParams):
+        msg = RegisterMessage(network_id=self.network_id, subnet_uid=self.subnet_uid, **params.model_dump())
+        msg.sign(signer)
+        return self._call_rpc('relayer.register', msg.model_dump())
+
+    def get_storage(self, signer: KeypairType):
+        msg = RelayerMessage(network_id=self.network_id, subnet_uid=self.subnet_uid)
+        msg.sign(signer)
+        return self._call_rpc('relayer.get_hotkey_storage', msg.model_dump())
+
+    def set_storage(self, signer: KeypairType, storage: MinerStorage | ValidatorStorage):
+        storage.sign(signer)
+        msg = StorageMessage(network_id=self.network_id, subnet_uid=self.subnet_uid, storage=storage.model_dump())
+        msg.sign(signer)
+        return self._call_rpc('relayer.set_hotkey_storage', msg.model_dump())
+
+    def perform_audit(self, signer: KeypairType, uid: int, code: str):
+        task = TaskModel(uid=uid, contract_code=code)
+        task.sign(signer)
+        msg = PerformAuditMessage(task=task.model_dump())
+        msg.sign(signer)
+        return self._call_rpc('miner.perform_audit', msg.model_dump())
---
+++ solidity_audit_lib/relayer_client/relayer_types.py
@@ -0,0 +1,51 @@
+import pydantic
+
+from solidity_audit_lib.messaging import SignedMessage
+
+
+__all__ = [
+    'RelayerMessage', 'RegisterParams', 'RegisterMessage', 'MinerStorage', 'ValidatorStorage', 'StorageMessage',
+    'TaskModel', 'PerformAuditMessage'
+]
+
+
+class RelayerMessage(SignedMessage):
+    network_id: int
+    subnet_uid: int
+
+
+class RegisterParams(pydantic.BaseModel):
+    uid: int
+    type: str
+    ip: str
+    port: int
+
+
+class RegisterMessage(RelayerMessage):
+    uid: int
+    type: str
+    ip: str
+    port: int
+
+
+class MinerStorage(SignedMessage):
+    collection_id: int
+
+
+class ValidatorStorage(SignedMessage):
+    last_validation: int
+    scores: dict[str, list[float | int]]
+    hotkeys: dict[str, str]
+
+
+class StorageMessage(RegisterMessage):
+    storage: dict
+
+
+class TaskModel(SignedMessage):
+    uid: int
+    contract_code: str
+
+
+class PerformAuditMessage(RegisterMessage):
+    task: TaskModel
